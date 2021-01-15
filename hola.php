75b9455839bf706ac4864d311fd33b603eb37111
Ivan-merchan <77493824+Ivan-merchan@users.noreply.github.com>
2021-01-15 08:18:07 -0500

    Create hola.php

---
+++ hola.php
@@ -0,0 +1,7 @@
+echo "# programa-hola-mundo" >> README.md 
+git init 
+git agregar README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote agregar origen https://github.com/Ivan-merchan/programa-hola-mundo.git
+ git push -u origin main
